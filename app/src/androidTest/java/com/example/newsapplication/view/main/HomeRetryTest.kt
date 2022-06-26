03bbd0caab4a8f1bd6735f52716ee144dadbd97d
tpsekhar <tpoorna.sekhar@capgemini.com>
2022-06-26 18:55:27 +0530

    Test case

--- app/src/androidTest/java/com/example/newsapplication/view/main/HomeRetryTest.kt
+++ app/src/androidTest/java/com/example/newsapplication/view/main/HomeRetryTest.kt
@@ -1,14 +1,9 @@
 package com.example.newsapplication.view.main
 
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -45,16 +40,4 @@ class HomeRetryTest {
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
-    @Before
-    fun B_retry() {
-        InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("svc wifi enable")
-        InstrumentationRegistry.getInstrumentation().uiAutomation.executeShellCommand("svc data enable")
-
-    }
-
-    @Test
-    fun B_onRetryCase() {
-        onView(withId(R.id.retry_btn)).perform(click())
-        onView(withId(R.id.news_rv)).check(matches(isDisplayed()))
-    }
 }
